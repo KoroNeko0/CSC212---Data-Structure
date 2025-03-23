7c578284b9d31b62055ed660d59861ce753cf0a8
Raghad11a <Raghad.a2004@gmail.com>
2025-03-23 20:14:42 +0300

    Update Main.java
    ضفت جمله طباعه

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
 
         System.out.println("Hello World :D! Can you edit this code ?");
         System.out.println("YUP");
+         System.out.println("hi maha");
     }//end main
 }//end class
