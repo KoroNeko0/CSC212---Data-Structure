83758d89e5b4659d85c7a30b727af9f283a6c43c
Mahadkx <mahachan03@gmail.com>
2025-03-23 21:06:26 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,6 +8,5 @@ public class Main {
         PrintClass obj = new PrintClass();
         obj.print1();
         obj.print2();
-        System.out.println("Hi Raghad i just used branch and a pull request, it seems complicated honsetly");
     }//end main
 }//end class
