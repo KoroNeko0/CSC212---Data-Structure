d5ec44468876067f2c47069ba0680ef0875d254f
Mahadkx <mahachan03@gmail.com>
2025-03-23 20:34:58 +0300

    Update Main.java
    added objects to test my other class "PrintClass"

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@ public class Main {
 
         System.out.println("Hello World :D! Can you edit this code ?");
         System.out.println("Kinda YES");
+
+        PrintClass obj = new PrintClass();
+        obj.print1();
+        obj.print2();
     }//end main
 }//end class
