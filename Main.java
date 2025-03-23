ea42163dc0468216d5b177493d195b1469c70691
Mahadkx <mahachan03@gmail.com>
2025-03-23 21:06:50 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World :D! Can you edit this code ?");
         System.out.println("YUP");
-         System.out.println("hi maha");
+        System.out.println("hi maha");
 
 
         PrintClass obj = new PrintClass();
         obj.print1();
         obj.print2();
-
+        System.out.println("Hi Raghad i just used branch and a pull request, it seems complicated honsetly");
     }//end main
 }//end class
