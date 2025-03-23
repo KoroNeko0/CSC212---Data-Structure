924709cfccae7f326a5e7095e505df503a8ca0be
KoroNeko0 <memo33300@gmail.com>
2025-03-23 22:31:25 +0300

    Added a print of "Hello world :D"

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello World :D! Can you edit this code ?");
+        System.out.println("YUP");
+        System.out.println("hi maha");
+
+
+        PrintClass obj = new PrintClass();
+        obj.print1();
+        obj.print2();
+
+        System.out.println("Hi Raghad i just used branch and a pull request, it seems complicated honsetly");
+
 
     }//end main
 }//end class
