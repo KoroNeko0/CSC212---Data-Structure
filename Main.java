892ef163ef0ba5d3773bc10f902f29d91df810f1
KoroNeko0 <memo33300@gmail.com>
2025-03-24 18:01:56 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,16 +10,9 @@ public class Main {
         obj.print2();
 
         System.out.println("Hi Raghad i just used branch and a pull request, it seems complicated honsetly");
-        
         System.out.println("Hi Maha, I'm trying to use git with eclipse");
-
         System.out.println("Hi");
 
-
-        System.out.println("And i'm using intelliJ IDEA :D i think it doesn't matter which editor we use");
-
-        System.out.println("Hi !!!!");
-        
 
     }//end main
 }//end class
