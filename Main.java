4b1d8ae677da08f68aecec8d310a709599af6a01
KoroNeko0 <memo33300@gmail.com>
2025-03-24 17:28:21 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello World :D! Can you edit this code ?");
+        System.out.println("YUP");
+        System.out.println("hi maha");
+
+
+        PrintClass obj = new PrintClass();
+        obj.print1();
+        obj.print2();
+
+        System.out.println("Hi Raghad i just used branch and a pull request, it seems complicated honsetly");
+        
+        System.out.println("Hi Maha, I'm trying to use git with eclipse");
+        System.out.println("Hi");
 
     }//end main
 }//end class
