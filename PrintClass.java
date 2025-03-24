5ea08165c4ced2fdec59ad60978add4dc215e3ed
Mahadkx <mahachan03@gmail.com>
2025-03-24 17:59:02 +0300

    Update PrintClass.java

--- PrintClass.java
+++ PrintClass.java
@@ -12,14 +12,3 @@ public class PrintClass {
         System.out.println("i use intelliJ idea");
     }
 }
-
-public class PrintClass {
-
-    public void print1(){
-        System.out.println("i love cats meow meow");
-    }
-    public void print2(){
-        System.out.println("my cat is naughty >:(");
-    }
-}
-
