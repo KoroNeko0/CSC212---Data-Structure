8ea6b050db413aa72711d90960df9fe2f68ce475
KoroNeko0 <memo33300@gmail.com>
2025-03-25 22:34:13 +0300

    Update Test.java

--- Main.java
+++ Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World :D! Can you edit this code ?");
-        System.out.println("YUP");
-        System.out.println("hi maha");
-
-
-        PrintClass obj = new PrintClass();
-        obj.print1();
-        obj.print2();
-        obj.print3();
-        System.out.println("Hi Raghad i just used branch and a pull request, it seems complicated honestly");
-        System.out.println("Hi Maha, I'm trying to use git with eclipse");
-        System.out.println("Hi Raghad, me too but instead i'm using IntelliJ IDEA");
+       
     }//end main
 }//end class
