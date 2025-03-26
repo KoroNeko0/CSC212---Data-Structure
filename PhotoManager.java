914d048083476f8c6bc28e5ca44859c971d43eda
KoroNeko0 <memo33300@gmail.com>
2025-03-26 07:15:00 +0300

    A new Class appeared

---
+++ PhotoManager.java
@@ -0,0 +1,23 @@
+public class PhotoManager {
+
+    public LinkedList<Photo> photosList;
+
+public PhotoManager(){
+
+    photosList = new LinkedList<Photo>();
+
+}
+
+public LinkedList<Photo> getPhotos(){
+    return photosList;
+}
+
+public void addPhoto(Photo p){
+    //Later
+}
+
+public void deletePhoto(String path){
+    //later
+}
+
+}//end class
