2d65d579c1d965a12a6b9f26c94b199772ddfb3f
Raghad <raghad.a2004@gmail.com>
2025-04-01 02:41:47 +0300

    .

---
+++ Album.java
@@ -0,0 +1,109 @@
+public class Album {
+
+    private String Name;
+    private String Condition;
+    private PhotoManager Manager;
+    private int NumComps;
+
+    public Album(String name, String condition, PhotoManager manager) {
+        Name = name;
+        Condition = condition;
+        Manager = manager;
+        NumComps = 0;
+
+    }
+
+    // Return the name of the album
+    public String getName() {
+        return Name;
+    }
+
+    // Return the condition associated with the album
+    public String getCondition() {
+        return Condition;
+    }
+
+    // Return the manager
+    public PhotoManager getManager() {
+        return Manager;
+    }
+
+    // Return all photos that satisfy the album condition
+    public LinkedList<Photo> getPhotos() {
+
+        LinkedList<Photo> Rphotos = new LinkedList<Photo>();
+        {
+            LinkedList<Photo> photos1 = Manager.getPhotos();
+            if (!photos1.empty()) {
+                photos1.findFirst();
+                while (!photos1.last()) {
+                    Rphotos.insert(new Photo(photos1.retrieve().getPath(), photos1.retrieve().getTags()));
+                    photos1.findNext();
+                }
+                Rphotos.insert(new Photo(photos1.retrieve().getPath(), photos1.retrieve().getTags()));
+            }
+        }
+
+        NumComps = 0;
+        if (this.Condition.compareTo("") != 0) {
+            String[] Array = Condition.split(" AND ");
+
+            Rphotos.findFirst();
+            while (!Rphotos.last()) {
+                Photo photo = Rphotos.retrieve();
+                if (!isTagAvailable(photo.getTags(), Array))
+                    Rphotos.remove();
+                else
+                    Rphotos.findNext();
+            }
+            Photo photo11 = Rphotos.retrieve();
+            if (!isTagAvailable(photo11.getTags(), Array))
+                Rphotos.remove();
+            else
+                Rphotos.findNext();
+        }
+        return Rphotos;
+
+
+    }
+
+    // Return the number of tag comparisons used to find all photos of the album
+    public int getNbComps() {
+        return NumComps;
+    }
+
+    private boolean isTagAvailable(LinkedList<String> AllTags, String[] ArrayC) {
+        if (AllTags.empty()) return false;
+
+        for (String Tag : ArrayC) {
+            boolean found = false;
+
+            AllTags.findFirst();
+            while (!AllTags.last()) {
+                NumComps++;
+
+                if (AllTags.retrieve().equalsIgnoreCase(Tag)) {
+                    found = true;
+                    break;
+                }
+
+                AllTags.findNext(); // الانتقال للعنصر التالي
+            }
+
+            if (!found) {
+                NumComps++;
+                if (AllTags.retrieve().equalsIgnoreCase(Tag)) {
+                    found = true;
+                }
+            }
+
+            if (!found) return false; // إذا لم نجد الوسم المطلوب، نُرجع false مباشرةً
+        }
+
+        return true; // جميع الوسوم المطلوبة وُجدت
+    }
+
+
+}
+
+
--- Test.java
+++ Test.java
@@ -3,51 +3,55 @@ public class Test {
 
         PhotoManager manager = new PhotoManager();
 
-        Photo photo1 = new Photo("hedgehog.jpg",toTagsLinkedList("animal, hedgehog, apple, grass, green"));
+        Photo photo1 = new Photo("hedgehog.jpg", toTagsLinkedList("animal, hedgehog, apple, grass, green"));
         manager.addPhoto(photo1);
-        Photo photo2 = new Photo("bear.jpg",toTagsLinkedList("animal, bear, cab, grass, wind"));
+        Photo photo2 = new Photo("bear.jpg", toTagsLinkedList("animal, bear, cab, grass, wind"));
         manager.addPhoto(photo2);
-        Photo photo3 = new Photo("orange-butterfly.jpg",toTagsLinkedList("insect, butterfly, flower, color"));
+        Photo photo3 = new Photo("orange-butterfly.jpg", toTagsLinkedList("insect, butterfly, flower, color"));
         manager.addPhoto(photo3);
 
         displayAllPhotos(manager);
 
-        System.out.println("After Deleting bear.jpg");
+        System.out.println("After Deleting hedgehog.jpg");
         manager.deletePhoto("hedgehog.jpg");
         displayAllPhotos(manager);
 
         Album album1 = new Album("Album1", "bear", manager);
         Album album2 = new Album("Album2", "animal AND grass", manager);
-        
+
         System.out.println("Get photo1 path and tags:");
         System.out.println("photo1 path: " + photo1.getPath());
-        
+
         //You can get the list of tags of photo1 by calling photo1.getTags().
+        LinkedList<String> Tags = photo1.getTags();
         //You can write a method that prints the list of tags of photo1.
-        
+        photo1.DisplayAllTags();
+
         System.out.println("Get album2 name, condition, and photos:");
         System.out.println("album2 name: " + album2.getName());
         System.out.println("album2 condition: " + album2.getCondition());
-        
+
         //You can get the list of photos in album2 by calling album2.getPhotos().
+        LinkedList<Photo> photos = album2.getPhotos();
         //You can write a method that prints the list of photos in album2.
-        
+        PrintAllPhotos(photos);// photo path in album2
+
         System.out.println("Delete the photo ’bear.jpg’:");
         manager.deletePhoto("bear.jpg");
-
+        displayAllPhotos(manager);
 
     }//end main
 
     private static LinkedList<String> toTagsLinkedList(String tags) {
-     LinkedList<String> result = new LinkedList<String>();
+        LinkedList<String> result = new LinkedList<String>();
 
         String[] tagsArray = tags.split("\\s*,\\s*");
 
-        for(int i = 0; i < tagsArray.length; i++) {
-        result.insert(tagsArray[i]);
+        for (int i = 0; i < tagsArray.length; i++) {
+            result.insert(tagsArray[i]);
         }//end for
 
-    return result;
+        return result;
     }
 
     public static void displayAllPhotos(PhotoManager pm) { //Displays all Photos in a specific Photo-Manager
@@ -60,7 +64,7 @@ public class Test {
 
             while (!allPhotos.last()) {
                 System.out.println("\nPhoto No." + (photoNum + 1) + ":");
-                System.out.println("Path: "+ allPhotos.retrieve().getPath());
+                System.out.println("Path: " + allPhotos.retrieve().getPath());
                 System.out.print("Tags: ");
                 allPhotos.retrieve().DisplayAllTags();
 
@@ -69,11 +73,28 @@ public class Test {
             }//end while
 
             System.out.println("\nPhoto No." + (photoNum + 1) + ":");
-            System.out.println("Path: "+ allPhotos.retrieve().getPath());
+            System.out.println("Path: " + allPhotos.retrieve().getPath());
             System.out.print("Tags: ");
             allPhotos.retrieve().DisplayAllTags();
 
         }//end if
+    }
+
+
+    private static void PrintAllPhotos(LinkedList<Photo> photos) {
+
+        if (!photos.empty()) {
+
+            photos.findFirst();
+
+            while (!photos.last()) {
+                System.out.println("Path: " + photos.retrieve().getPath());
+                photos.findNext();
+            }//end while
+            System.out.println("Path: " + photos.retrieve().getPath());
+
+        }//end if
+
     }
 
 }//end class
